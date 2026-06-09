9e015fec18f235e05fd58e7cb1d7e9cfa1be9e09
vercelvo6-arch <vercelvo6@gmail.com>
2026-06-08 21:08:58 -0500

    Update entregador-view.tsx

--- components/entregador-view.tsx
+++ components/entregador-view.tsx
@@ -1149,8 +1149,8 @@ export function EntregadorView({ onLogout, user }: EntregadorViewProps) {
                   })
                 )}
               </div>
-            </main>
-          </div>
+          </main>
+        </div>
 
       {/* Modal de novedad unificado (Fiado / Devolución / Agotado) */}
       <Dialog open={showNovedadModal} onOpenChange={setShowNovedadModal}>
